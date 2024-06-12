62a63eb27605db8b10efd520b678adc9e6cf33f1
Deeparekha Ramanujam <Deeparekha.Ramanujam@tesco.com>
2024-06-12 11:45:45 +0530

    main changes1

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+
+        System.out.printf("Hello sand welcome!");
+        System.out.println("Welcomess git and intellij");
+        System.out.println("Welcomes sgit and intellij via github");
+        System.out.println("Welcomes git and intellijv2 via github");
+        for (int i = 1; i <= 5; i++) {
+            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
+            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
+            System.out.println("i = " + i);
+        }
+    }
+}
